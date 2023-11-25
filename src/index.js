ee42f4b80e98f73ddd7d07b66580cf001bb539f7
falshami2002 <alshamifiras@yahoo.com>
2023-11-24 20:50:16 -0800

    Revert "Updates"
    This reverts commit 352e61beab245b6bf0e5398541bb71a0aba75dee.

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Game from './components/Game';
+import Calculator from './components/Calculator';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Game />
+    <Calculator />
+  </React.StrictMode>
+);
